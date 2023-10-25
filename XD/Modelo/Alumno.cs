6476520818e39aed4e7e972fa8e915d4bb7fa04e
Natanael-YM23003 <143116361+Natanael-YM23003@users.noreply.github.com>
2023-10-25 15:32:44 -0600

    Add project files.

---
+++ XD/Modelo/Alumno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD.Modelo
+{
+    class Alumno
+    {
+        public string Numero_Carnet { get; set; }
+        public string Nombre { get; set; }
+        public double[] Notas { get; set; }
+
+        public double Promedio { get; set; }
+        public void Guardar_Datos()
+        {
+            this.Notas = new double[4];
+            Console.Write("Ingrese el número de carnet: ");
+            this.Numero_Carnet = Console.ReadLine();
+            Console.Write("Ingrese el nombre: ");
+            this.Nombre = Console.ReadLine();
+            for (int i = 0; i < this.Notas.Length; i++)
+            {
+                Console.WriteLine($"Ingrese la nota {i+1}");
+                Notas[i] = double.Parse(Console.ReadLine());
+            }
+            Calcular_Promedio();
+        }
+        public void Calcular_Promedio()
+        {
+            double Suma = 0;
+            for (int i = 0; i < this.Notas.Length; i++)
+            {
+                Suma += Notas[i];
+            }
+            this.Promedio = Suma / this.Notas.Length;
+        }
+        public void Mostrar_Datos()
+        {
+            Console.WriteLine($"Numero de carnet -> {this.Numero_Carnet}");
+            Console.WriteLine($"Nombre -> {this.Nombre}");
+            Console.WriteLine($"Promedio -> {this.Promedio}");
+            Console.WriteLine(this.Promedio < 6? "Reprobado" : "Aprobado");
+            Console.WriteLine("Notas:");
+            for (int i = 0; i < this.Notas.Length; i++)
+            {
+                Console.WriteLine($"\t {Notas[i]}");
+            }
+        }
+
+    }
+}
---
+++ XD/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using XD.Modelo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string Materia;
+        string Numero_Grupo;
+        string Condición = "";
+        Dictionary<string, Dictionary<string, List<Alumno>>> Matricula;
+        Matricula = new Dictionary<string, Dictionary<string, List<Alumno>>>();
+        do
+        {
+            Dictionary<string, List<Alumno>> Diccionario_Grupos;
+            Diccionario_Grupos = new Dictionary<string, List<Alumno>>();
+            Console.Write("Ingrese el nombre de la materia :");
+            Materia = Console.ReadLine();
+            do
+            {
+                List<Alumno> Lista_Alumnos = new List<Alumno>();
+                Console.WriteLine($"Ingrese el numero de grupo para materia {Materia}");
+                Numero_Grupo = Console.ReadLine();
+                do
+                {
+                    Alumno Alumno_Item = new Alumno();
+                    Alumno_Item.Guardar_Datos();
+                    Lista_Alumnos.Add(Alumno_Item);
+                } while (Validar_Condición("Alumno"));
+
+                Diccionario_Grupos.Add(Numero_Grupo, Lista_Alumnos);
+
+            } while (Validar_Condición("Grupo"));
+
+            Matricula.Add(Materia, Diccionario_Grupos);
+        } while (Validar_Condición("Materia"));
+
+        foreach (var Item_Matricula in Matricula)
+        {
+            Console.WriteLine($"Materia -> {Item_Matricula.Key}");
+            foreach (var Item_Grupos in Item_Matricula.Value)
+            {
+                Console.WriteLine($"Grupo -> {Item_Grupos.Key}");
+                foreach (var Item_Alumnos in Item_Grupos.Value)
+                {
+                    Item_Alumnos.Mostrar_Datos();
+                }
+            }
+        }
+    }
+
+    public static bool Validar_Condición(string Mensaje)
+    {
+        string Condición = "";
+        Console.WriteLine($"Si desea ingresar otro {Mensaje}, presione s para si\ncualquier letra para no");
+        Condición = Console.ReadLine();
+        if (Condición.Equals("s", StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
